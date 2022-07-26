fd94ebed453ada0342f01425da11264443a80c30
JagadishNaidu1994 <50915479+JagadishNaidu1994@users.noreply.github.com>
2022-07-26 18:54:05 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,10 +1,10 @@
 def buildWar() {
-    echo "building the application..."
+    echo "Building the application..."
     sh 'mvn install'
 } 
 
 def deployApp() {
-    echo 'deploying the application...'
+    echo 'Deploying the application...'
     sshagent(['deploy-test']) {
                 sh "scp -o StrictHostKeyChecking=no gameoflife-web/target/gameoflife.war ec2-user@54.218.218.31:/opt/apache-tomcat-9.0.64/webapps"
                 }
