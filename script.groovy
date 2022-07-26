d7368f64c1198e2ea30c2fb48f02631f2dd7d687
JagadishNaidu1994 <50915479+JagadishNaidu1994@users.noreply.github.com>
2022-07-26 19:48:57 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -7,6 +7,7 @@ def deployApp() {
     echo 'Deploying the application...'
     sshagent(['deploy-test']) {
                 sh "scp -o StrictHostKeyChecking=no gameoflife-web/target/gameoflife.war ec2-user@54.218.218.31:/opt/apache-tomcat-9.0.64/webapps"
+                sh "//opt/apache-tomcat-9.0.64/bin/startup.sh start"
                 }
 } 
 return this
