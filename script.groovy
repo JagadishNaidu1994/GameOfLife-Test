5a07a94573c12c577f2671e717f7bac1223ede32
JagadishNaidu1994 <50915479+JagadishNaidu1994@users.noreply.github.com>
2022-07-26 19:02:22 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -9,5 +9,3 @@ def deployApp() {
                 sh "scp -o StrictHostKeyChecking=no gameoflife-web/target/gameoflife.war ec2-user@54.218.218.31:/opt/apache-tomcat-9.0.64/webapps"
                 }
 } 
-
-return this
